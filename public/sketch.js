e0684a6dd16d755e60f2be102ec79b92691e7e2f
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-09-13 21:43:47 -0400

    strudying shape from generative design book

--- public/sketch.js
+++ public/sketch.js
@@ -5,12 +5,26 @@ function setup() {
 	background(210, 23, 92);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
-	console.log(width);
 }
 
 function draw() {
-	stroke(0, 0, 0, 10);
-	strokeWeight(width / width);
-	noSmooth();
-	ellipse(width / 2, height / 2, width / 20, width / 20);
+	background(255, 100);
+	translate(width / 2, height / 2);
+	let circleResolution = map(mouseY, 0, height, 20, 800);
+	let radius = mouseX - width / 2 + 0.5;
+	let angle = TWO_PI / circleResolution;
+	let xoff = 3;
+
+	strokeWeight(mouseY / 50);
+
+	beginShape();
+	noFill();
+	for (let i = 0; i <= circleResolution; i++) {
+		let x = 0 + cos(angle * i) * radius;
+		let y = 0 + sin(angle * i) * radius;
+		let yoff = random(y / 10);
+		line(0, 0, x + xoff, y + yoff);
+		curveVertex(x + xoff, y + yoff);
+	}
+	endShape(CLOSE);
 }
