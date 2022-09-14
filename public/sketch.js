91e21cf37fe12ef1161c917fe333f9e3fc1ee743
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-09-14 15:03:05 -0400

    added new swatches and scanlines

--- public/sketch.js
+++ public/sketch.js
@@ -7,32 +7,32 @@ let ballW = 0;
 let ballH = 0;
 let ball = [];
 function preload() {
-	for (i = 0; i <= 6; i++) {
+	for (i = 0; i <= 12; i++) {
 		swatches[i] = loadImage('./assets/grid_' + i + '.jpg');
 	}
 }
 
 function setup() {
 	pixelDensity(3);
-	createCanvas(300, 1166);
+	createCanvas(1000, 1000);
 	rectMode(CENTER);
 	background(245);
-	for (i = 0; i <= 6; i++) {
+	for (i = 0; i <= 12; i++) {
 		swatchesLength[i] = swatches[i].height;
 	}
-	for (i = 0; i <= width * 6; i++) {
+	for (i = 0; i <= height * 6; i++) {
 		if (i % 5 == 0) {
-			ballW = width / 400;
-			ballH = width / 50;
-			ball[i] = new Walker(swatches[0], swatchesLength[0], ballW, ballH);
+			ballW = width / 300;
+			ballH = width / 40;
+			ball[i] = new Walker(swatches[10], swatchesLength[10], ballW, ballH);
 		} else if (i % 6 == 0) {
-			ballW = width / 50;
-			ballH = width / 400;
-			ball[i] = new Walker(swatches[1], swatchesLength[1], ballW, ballH);
+			ballW = width / 40;
+			ballH = width / 300;
+			ball[i] = new Walker(swatches[11], swatchesLength[11], ballW, ballH);
 		} else {
-			ballW = width / 100;
-			ballH = width / 100;
-			ball[i] = new Walker(swatches[2], swatchesLength[2], ballW, ballH);
+			ballW = width / 70;
+			ballH = width / 70;
+			ball[i] = new Walker(swatches[12], swatchesLength[12], ballW, ballH);
 		}
 	}
 
@@ -40,6 +40,17 @@ function setup() {
 		for (step = 0; step < 1000; step++) {
 			ball[i].display();
 			ball[i].move();
+		}
+	}
+
+	makeScanlines();
+}
+
+function makeScanlines() {
+	for (i = 0; i < width; i++) {
+		if (i % 2 == 0) {
+			stroke(0, 0, 100, 100);
+			line(i, 0, i, height);
 		}
 	}
 }
