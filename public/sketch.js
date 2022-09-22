4f18b06bfe0fb58250db33ca814fe178c44526af
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-09-22 17:56:11 -0400

    final

--- public/sketch.js
+++ public/sketch.js
@@ -7,7 +7,7 @@ let ballW = 0;
 let ballH = 0;
 let ball = [];
 function preload() {
-	for (i = 0; i <= 12; i++) {
+	for (i = 0; i <= 15; i++) {
 		swatches[i] = loadImage('./assets/grid_' + i + '.jpg');
 	}
 }
@@ -17,22 +17,22 @@ function setup() {
 	createCanvas(12 * 100, 18 * 100);
 	rectMode(CENTER);
 	background(245);
-	for (i = 0; i <= 12; i++) {
+	for (i = 0; i <= 15; i++) {
 		swatchesLength[i] = swatches[i].height;
 	}
 	for (i = 0; i <= height * 6; i++) {
 		if (i % 5 == 0) {
 			ballW = width / 300;
 			ballH = width / 40;
-			ball[i] = new Walker(swatches[10], swatchesLength[10], ballW, ballH);
+			ball[i] = new Walker(swatches[13], swatchesLength[13], ballW, ballH);
 		} else if (i % 6 == 0) {
 			ballW = width / 40;
 			ballH = width / 300;
-			ball[i] = new Walker(swatches[11], swatchesLength[11], ballW, ballH);
+			ball[i] = new Walker(swatches[15], swatchesLength[15], ballW, ballH);
 		} else {
 			ballW = width / 70;
 			ballH = width / 70;
-			ball[i] = new Walker(swatches[12], swatchesLength[12], ballW, ballH);
+			ball[i] = new Walker(swatches[14], swatchesLength[14], ballW, ballH);
 		}
 	}
 
