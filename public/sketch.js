151fe2a20b4d4e441b3e0c5213b87c5eeff86b4a
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-09-29 16:49:43 -0400

    fixed landscape mode

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 	pixelDensity(3.0);
-	createCanvas(1000, 1000);
+	createCanvas(1920, 1000);
 	colorMode(HSB, 360, 100, 100, 100);
 	background(210, 23, 92);
 	randomSeed(fxrand() * 10000);
