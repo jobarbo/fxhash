8c6006cabd13abdbad402ec7eb2e3ffcb548438b
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-10-13 21:45:45 -0400

    sine + noise mountains

--- public/sketch.js
+++ public/sketch.js
@@ -1,15 +1,29 @@
+let xoffStart = 0;
+
+let xoffInc = 0.003;
+
 function setup() {
 	pixelDensity(3.0);
-	createCanvas(1920, 1000);
+	createCanvas(1000, 1000);
 	colorMode(HSB, 360, 100, 100, 100);
-	background(210, 23, 92);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
 }
 
 function draw() {
-	stroke(0, 0, 0, 10);
-	strokeWeight(width / width);
-	noSmooth();
-	ellipse(width / 2, height / 2, width / 20, width / 20);
+	background(210, 23, 10);
+	noFill();
+	beginShape();
+	stroke(0, 0, 70, 100);
+	strokeWeight(1);
+	let xoff = xoffStart;
+	for (let x = -100; x <= width + 100; x++) {
+		let n = map(noise(xoff), 0, 1, -400, 400);
+		let s = map(sin(xoff), -1, 1, 400, height - 400);
+		let y = s + n;
+		vertex(x, y);
+		xoff += xoffInc;
+	}
+	endShape();
+	xoffStart += xoffInc;
 }
