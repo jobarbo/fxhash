dc120d1a5245411532ad755c44581dd065c77a38
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-10-14 10:51:51 -0400

    removed basis

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,7 @@
+let inc = 0.01;
 function setup() {
 	pixelDensity(3.0);
-	createCanvas(1920, 1000);
+	createCanvas(1000, 1000);
 	colorMode(HSB, 360, 100, 100, 100);
 	background(210, 23, 92);
 	randomSeed(fxrand() * 10000);
@@ -8,8 +9,10 @@ function setup() {
 }
 
 function draw() {
-	stroke(0, 0, 0, 10);
-	strokeWeight(width / width);
-	noSmooth();
-	ellipse(width / 2, height / 2, width / 20, width / 20);
+	loadPixels();
+	background(51);
+	stroke(255);
+	noFill();
+	beginShape();
+	endShape();
 }
