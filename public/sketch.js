987b96e2a893e4e7d6a87c3900676b34f54ff009
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-05 15:33:25 -0500

    added save canvas module

--- public/sketch.js
+++ public/sketch.js
@@ -13,15 +13,3 @@ function draw() {
 	noSmooth();
 	ellipse(width / 2, height / 2, width / 20, width / 20);
 }
-
-function keyPressed() {
-	if (key == 's' || key == 'S') {
-		saveArtwork();
-	}
-}
-
-// make a function to save the canvas as a png file with the git branch name and a timestamp
-function saveArtwork() {
-	var timestamp = Date.now();
-	save('artwork' + '-' + timestamp + '.png');
-}
