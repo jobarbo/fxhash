0d06cf50f5137ca137439664c24783e150a2c72b
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-05 17:15:46 -0500

    new base

---
+++ public/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+	pixelDensity(2.0);
+	createCanvas(1000, 1000);
+	colorMode(HSB, 360, 100, 100, 100);
+	background(210, 23, 92);
+	randomSeed(fxrand() * 10000);
+	noiseSeed(fxrand() * 10000);
+}
+
+function draw() {
+	stroke(0, 0, 0, 10);
+	strokeWeight(width / width);
+	noSmooth();
+	ellipse(width / 2, height / 2, width / 20, width / 20);
+}
