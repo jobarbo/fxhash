304ab0976e5e49527edf47ef424bf608e18f05f8
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-16 17:06:43 -0500

    Added new utils function and console.loging(obscvrvm)

--- src/index.js
+++ src/index.js
@@ -19,3 +19,28 @@
 //   "Number of lines": 10,
 //   "Inverted": true
 // }
+//* COMPOSITION GENERATION *//
+
+let composition_params;
+
+composition_params = generate_composition_params();
+
+var {center_piece_type} = composition_params; // unpacking parameters we need in main.js and turning them into globals
+
+//* FXHASH FEATURES DEFINITION *//
+window.$fxhashFeatures = {
+	'Center piece type': center_piece_type,
+};
+
+//* CONSOLE LOGGING *//
+
+var jbarbeau_logo = '%c                                                                         \n' + '%c     Art by Jonathan Barbeau  |  { @jbarbeau.art }  |  2022              \n' + '%c                                                                         \n';
+
+console.log(jbarbeau_logo, 'color: white; background: #000000; font-weight: bold; font-family: "Courier New", monospace;', 'color: white; background: #000000; font-weight: bold; font-family: "Courier New", monospace;', 'color: white; background: #000000; font-weight: bold; font-family: "Courier New", monospace;');
+console.log('%cLa nuit porte... de garage\n', 'font-style: italic; font-family: "Courier New", monospace;');
+
+console.log('%cTOKEN FEATURES', 'color: white; background: #000000;', '\n', 'Center piece type -> ' + center_piece_type, '\n');
+
+console.log('%cCONTROLS', 'color: white; background: #000000;', '\n', 'cmd + s   : save artwork with date', '\n');
+
+//* END CONSOLE LOGGING *//
