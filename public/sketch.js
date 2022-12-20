ba458840c8a779279d0225fd1af5b7ab686376cd
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-20 16:02:59 -0500

    added some post_proc for fun

--- public/sketch.js
+++ public/sketch.js
@@ -2,7 +2,7 @@ function setup() {
 	pixelDensity(2);
 	createCanvas(1000, 1000);
 	colorMode(HSB, 360, 100, 100, 100);
-	background(45, 13, 92);
+	background(50, 5, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
 
