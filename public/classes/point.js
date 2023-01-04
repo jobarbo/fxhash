13cfd04084a8caa5c5d21804982ba853463878e8
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-01-04 17:43:49 -0500

    got something

--- public/classes/point.js
+++ public/classes/point.js
@@ -1,12 +1,32 @@
 class Intersection {
 	constructor(position) {
 		this.position = position;
-		this.edges = [];
-		this.edges.push(this);
+		this.intersected = false;
+		this.lineHue = 0;
 	}
 
 	show() {
-		fill(255);
+		fill(255, 0);
+		stroke(0, 0, 0, 0);
 		ellipse(this.position.x, this.position.y, 10, 10);
+	}
+
+	move() {
+		this.position.x += random(-2, 2);
+		this.position.y += random(-2, 2);
+	}
+
+	isTouching(i, j) {
+		this.lineHue += random(0, 10);
+		if (this.lineHue > 360) {
+			this.lineHue = 0;
+		} else if (this.lineHue < 0) {
+			this.lineHue = 360;
+		}
+		stroke(this.lineHue, 20, 100, 5);
+		line(points[i].position.x, points[i].position.y, points[j].position.x, points[j].position.y);
+		ellipse(points[i].position.x, points[i].position.y, 10, 10);
+		points[i].intersected = true;
+		points[j].intersected = true;
 	}
 }
--- public/sketch.js
+++ public/sketch.js
@@ -1,26 +1,40 @@
+let points = [];
+
 function setup() {
 	pixelDensity(2.0);
 	createCanvas(1000, 1000);
 	colorMode(HSB, 360, 100, 100, 100);
-	background(210, 23, 92);
+	background(210, 23, 12);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
 
 	// create intersection points with the point class
-	var points = [];
-	for (var i = 0; i < 100; i++) {
-		points.push(new Point(random(width), random(height)));
+	for (var i = 0; i < 500; i++) {
+		// create a vector with random x and y values
+		let position = createVector(random(100, width - 100), random(100, height - 100));
+		points.push(new Intersection(position));
 	}
 
 	// show the points
-	for (var i = 0; i < points.length; i++) {
-		points[i].show();
-	}
 }
 
 function draw() {
-	stroke(0, 0, 0, 10);
-	strokeWeight(width / width);
-	noSmooth();
-	ellipse(width / 2, height / 2, width / 20, width / 20);
+	//background(210, 23, 92);
+	for (var i = 0; i < points.length; i++) {
+		points[i].show();
+		points[i].move();
+
+		// check if the point is intersecting with any other points
+		for (var j = 0; j < points.length; j++) {
+			// if the point is intersecting with itself, skip it
+			if (i == j) {
+				continue;
+			}
+
+			// if the point is intersecting with another point, draw a line between them
+			if (dist(points[i].position.x, points[i].position.y, points[j].position.x, points[j].position.y) < 50) {
+				points[i].isTouching(i, j);
+			}
+		}
+	}
 }
