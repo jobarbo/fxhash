e4ad15fd2a502a05678753737a5253e558a6fb04
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-03-17 10:30:28 -0400

    testing px density

--- public/shapes/rect.js
+++ public/shapes/rect.js
@@ -94,11 +94,6 @@ class Rect {
 		pop();
 		// create a new canvas graphics the same size as the canvas to draw textures on
 		this.mask = createGraphics(width, height);
-		if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-			this.mask.pixelDensity(2.0);
-		} else {
-			this.mask.pixelDensity(3.0);
-		}
 		this.mask.colorMode(HSB, 360, 100, 100, 100);
 		this.mask.rectMode(CENTER);
 		this.mask.background(this.color);
--- public/sketch.js
+++ public/sketch.js
@@ -25,11 +25,8 @@ function setup() {
 
 	let formatMode = features.format_mode;
 	// if safari mobile use pixelDensity(2.0) to make the canvas bigger else use pixelDensity(3.0)
-	if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-		pixelDensity(2.0);
-	} else {
-		pixelDensity(3.0);
-	}
+
+	pixelDensity(3.0);
 	createCanvas(format[formatMode][0], format[formatMode][1]);
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
