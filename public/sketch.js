dc8eece37bcebcc78ab7ed48d738c18e904cc5c8
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-03-31 15:43:38 -0400

    new base

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,8 @@
+let horizon = '';
+let ellipseX = '';
+let ellipseY = '';
+let ellipseR = '';
+let horizon_lineArr = [];
 function setup() {
 	pixelDensity(2.0);
 	createCanvas(1000, 1000);
@@ -5,12 +10,56 @@ function setup() {
 	background(40, 5, 10);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
+
+	horizon_lineArr = [
+		0,
+		height / 1.05,
+		height / 1.1,
+		height / 1.2,
+		height / 1.35,
+		height / 1.5,
+		height / 1.75,
+		height / 2,
+		height / 2.5,
+		height / 3,
+		height / 4,
+		height / 5,
+
+		height / 8,
+
+		height / 13,
+
+		height / 25,
+
+		height / 80,
+
+		height,
+	];
+	let horizon_line = random(horizon_lineArr);
+
+	ellipseX = random(width);
+	ellipseY = random(horizon_line);
+	ellipseR = 100;
+
+	console.log('horizon line: ' + horizon_line);
+	horizon = new Horizon(horizon_line);
 }
 
 function draw() {
 	stroke(0, 0, 100, 100);
-	strokeWeight(width / width);
-	fill(0, 0, 100, 100);
-	noSmooth();
-	ellipse(width / 2, height / 2, width / 20, width / 20);
+
+	strokeWeight(3);
+
+	horizon.draw();
+
+	// draw a rect at the horizon line full width that goes to the bottom of the canvas
+	fill(111, 30, 100, 100);
+	rect(0, horizon.y, width, height);
+
+	// always draw the ellipse on the top half of the canvas
+
+	noStroke();
+	fill(325, 100, 100, 100);
+
+	ellipse(ellipseX, ellipseY - ellipseR / 2, 100, 100);
 }
