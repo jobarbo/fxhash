1f6f6c7e34a5858df70765291baaf005be9133b7
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-04 14:33:05 -0500

    updated?

--- project/src/index.js
+++ project/src/index.js
@@ -9,32 +9,42 @@ let composition_params;
 composition_params = generate_composition_params();
 //console.log(composition_params);
 
-var {shape_type, ellipse_num, line_num, rectangle_num, bg_mode, border_mode, format_mode, palette_mode, angle_mode} =
-	composition_params; // unpacking parameters we need in main.js and turning them into globals
+var {
+	shape_type,
+	ellipse_num,
+	line_num,
+	rectangle_num,
+	bg_mode,
+	border_mode,
+	format_mode,
+	palette_mode,
+	angle_mode,
+} = composition_params; // unpacking parameters we need in main.js and turning them into globals
 
 //console.log(shape_type, ellipse_num, line_num, rectangle_num, bg_mode, border_mode, format_mode, palette_mode, angle_mode);
 // this is how to define parameters
+console.log(shape_type);
 $fx.params([
 	{
-		id: 'shape_type',
-		name: 'Type of',
-		type: 'select',
-		//default: Math.PI,
+		id: "shape_type",
+		name: "Type of",
+		type: "select",
+		default: "ellipse",
 		options: {
 			options: [
-				'ellipse',
-				'rectangle',
-				'ellipse & line',
-				'ellipse & rectangle',
-				'line & rectangle',
-				'ellipse & line & rectangle',
+				"ellipse",
+				"rectangle",
+				"ellipse & line",
+				"ellipse & rectangle",
+				"line & rectangle",
+				"ellipse & line & rectangle",
 			],
 		},
 	},
 	{
-		id: 'ellipse_num',
-		name: 'Ellipse num',
-		type: 'number',
+		id: "ellipse_num",
+		name: "Ellipse num",
+		type: "number",
 		//default: Math.PI,
 		options: {
 			// make a shorthand if to check if the ellipse num is 0
@@ -44,9 +54,9 @@ $fx.params([
 		},
 	},
 	{
-		id: 'line_num',
-		name: 'Lines num',
-		type: 'number',
+		id: "line_num",
+		name: "Lines num",
+		type: "number",
 		//default: Math.PI,
 		options: {
 			min: 3,
@@ -55,9 +65,9 @@ $fx.params([
 		},
 	},
 	{
-		id: 'rect_num',
-		name: 'Rect num',
-		type: 'number',
+		id: "rect_num",
+		name: "Rect num",
+		type: "number",
 		//default: Math.PI,
 		options: {
 			min: 1,
@@ -66,67 +76,73 @@ $fx.params([
 		},
 	},
 	{
-		id: 'bg_mode',
-		name: 'Background mode',
-		type: 'select',
+		id: "bg_mode",
+		name: "Background mode",
+		type: "select",
 		//default: Math.PI,
 		options: {
-			options: ['light', 'dark'],
+			options: ["light", "dark"],
 		},
 	},
 	{
-		id: 'border_mode',
-		name: 'Border mode',
-		type: 'select',
+		id: "border_mode",
+		name: "Border mode",
+		type: "select",
 		//default: Math.PI,
 		options: {
-			options: ['none', 'border'],
+			options: ["none", "border"],
 		},
 	},
 	{
-		id: 'format_mode',
-		name: 'Format mode',
-		type: 'select',
+		id: "format_mode",
+		name: "Format mode",
+		type: "select",
 		//default: Math.PI,
 		options: {
-			options: ['portrait', 'landscape', 'square'],
+			options: ["portrait", "landscape", "square"],
 		},
 	},
 	{
-		id: 'palette_mode',
-		name: 'Palette mode',
-		type: 'select',
+		id: "palette_mode",
+		name: "Palette mode",
+		type: "select",
 		//default: Math.PI,
 		options: {
-			options: ['80s', '90s', 'june', 'mango', 'traditional', 'mono'],
+			options: ["80s", "90s", "june", "mango", "traditional", "mono"],
 		},
 	},
 	{
-		id: 'angle_mode',
-		name: 'Angle mode',
-		type: 'select',
+		id: "angle_mode",
+		name: "Angle mode",
+		type: "select",
 		//default: Math.PI,
 		options: {
-			options: ['random', 'straight', 'diagonal', 'diagonal_45', 'diagonal_135'],
+			options: [
+				"random",
+				"straight",
+				"diagonal",
+				"diagonal_45",
+				"diagonal_135",
+			],
 		},
 	},
 ]);
-console.log($fx.getParam('shape_type'));
+console.log($fx.getParam("shape_type"));
 // this is how features can be defined
 $fx.features({
-	shape_type: $fx.getParam('shape_type'),
-	ellipse_num: $fx.getParam('ellipse_num'),
-	line_num: $fx.getParam('line_num'),
-	rectangle_num: $fx.getParam('rect_num'),
-	bg_mode: $fx.getParam('bg_mode'),
-	border_mode: $fx.getParam('border_mode'),
-	format_mode: $fx.getParam('format_mode'),
-	palette_mode: $fx.getParam('palette_mode'),
-	angle_mode: $fx.getParam('angle_mode'),
+	shape_type: $fx.getParam("shape_type"),
+	ellipse_num: $fx.getParam("ellipse_num"),
+	line_num: $fx.getParam("line_num"),
+	rectangle_num: $fx.getParam("rect_num"),
+	bg_mode: $fx.getParam("bg_mode"),
+	border_mode: $fx.getParam("border_mode"),
+	format_mode: $fx.getParam("format_mode"),
+	palette_mode: $fx.getParam("palette_mode"),
+	angle_mode: $fx.getParam("angle_mode"),
 });
 
 // log the parameters, for debugging purposes, artists won't have to do that
-console.log('Current param values:');
+console.log("Current param values:");
 
 // Added addtional transformation to the parameter for easier usage
 // e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0]
